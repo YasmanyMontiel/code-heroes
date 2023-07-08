7e794244cf4ba9c132526a019e70060d1d991c0d
Yasmany Montiel Rivera <yasmontiel87@gmail.com>
2023-07-08 20:05:45 +0200

    add module, routing  and components to the feature heroe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'' , loadChildren: () => import('./heroe/heroe.module').then(m => m.HeroeModule)},
+  {path: '' , redirectTo: '/' , pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
